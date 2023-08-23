27986bb9ed3c9c44f61680e3172a5d6b787b36b5
Alberto-1992 <beto_1866@outlook.com>
2023-08-23 10:06:43 -0600

    carga de tabla de busqueda actualizada

--- consultadataTable.php
+++ consultadataTable.php
@@ -1,7 +1,7 @@
 <?php 
 $status = $_POST['status'];
         require 'conexionRh.php';
-        $sql =$conexionRh->prepare("SELECT personaloperativo2023.id_empleado,personaloperativo2023.rfc as rfcempleado, personaloperativo2023.nombre as nombretrabajador,personaloperativo2023.descripcionestructura2,personaloperativo2023.correo as correoempleado, jefes2022.nombre as nombrejefe, jefes2022.correo as correojefe from personaloperativo2023 inner join jefes2022 on jefes2022.id_jefe=personaloperativo2023.id_jefe where personaloperativo2023.eliminado = :eliminado and vistobueno=:vistobueno");
+        $sql =$conexionRh->prepare("SELECT personaloperativo2023.id_empleado,personaloperativo2023.rfc as rfcempleado, personaloperativo2023.nombre as nombretrabajador, personaloperativo2023.apellidopaterno, personaloperativo2023.apellidomaterno,personaloperativo2023.descripcionestructura2,personaloperativo2023.correo as correoempleado, jefes2022.nombre as nombrejefe, jefes2022.correo as correojefe, jefes2022.rfc as rfcjefe from personaloperativo2023 inner join jefes2022 on jefes2022.id_jefe=personaloperativo2023.id_jefe where personaloperativo2023.eliminado = :eliminado and vistobueno=:vistobueno");
             $sql->execute(array(
                 ':eliminado'=>0,
                 ':vistobueno'=>$status
@@ -18,6 +18,7 @@ $status = $_POST['status'];
                 <th>R.F.C</th>
                 <th>Nombre jefe</th>
                 <th>Correo del jefe</th>
+                <th>R.F.C del jefe</th>
             </tr>
         </thead>
         <tbody>
@@ -26,12 +27,13 @@ $status = $_POST['status'];
                 ?>
             <tr>
                 <td><?php echo $dataRegistro['id_empleado'] ?></td>
-                <td><?php echo $dataRegistro['nombretrabajador'] ?></td>
+                <td><?php echo $dataRegistro['nombretrabajador'].' '.$dataRegistro['apellidopaterno'].' '.$dataRegistro['apellidomaterno'] ?></td>
                 <td><?php echo $dataRegistro['descripcionestructura2'] ?></td>
                 <td><?php echo $dataRegistro['correoempleado'] ?></td>
                 <td><?php echo $dataRegistro['rfcempleado'] ?></td>
                 <td><?php echo $dataRegistro['nombrejefe'] ?></td>
                 <td><?php echo $dataRegistro['correojefe'] ?></td>
+                <td><?php echo $dataRegistro['rfcjefe'] ?></td>
             </tr>
             <?php
             }
@@ -47,6 +49,7 @@ $status = $_POST['status'];
                 <th>R.F.C</th>
                 <th>Nombre jefe</th>
                 <th>Correo del jefe</th>
+                <th>R.F.C del jefe</th>
             </tr>
         </tfoot>
     </table>
