e3eee00cefd05898195bd1273d50832d727efe05
Talento RRHH <talentorh@hraei.gob.mx>
2023-10-26 21:44:54 -0600

    carga de graficos area medica y enfermeria

--- graficos/graficosCabecera.php
+++ graficos/graficosCabecera.php
@@ -288,173 +288,7 @@
 <div class="graff" id="grafico_lineal">
     <!-- Styles -->
     <style>
-        #chartdiv3 {
-            width: 100%;
-            height: 15rem;
-            font-size: 10px;
-        }
-    </style>
-    <div class="titulo">
-        <h2 style="font-size: 13px;">Bajas 2023</h2>
-    </div>
-    <!-- Resources -->
-    <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
-    <script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
-    <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
-
-    <!-- Chart code -->
-    <script>
-am5.ready(function() {
-
-
-// Create root element
-// https://www.amcharts.com/docs/v5/getting-started/#Root_element
-var root = am5.Root.new("chartdiv3");
-
-
-var myTheme = am5.Theme.new(root);
-
-myTheme.rule("Grid", ["base"]).setAll({
-strokeOpacity: 0.1
-});
-
-
-// Set themes
-// https://www.amcharts.com/docs/v5/concepts/themes/
-root.setThemes([
-am5themes_Animated.new(root),
-myTheme
-]);
-
-
-// Create chart
-// https://www.amcharts.com/docs/v5/charts/xy-chart/
-var chart = root.container.children.push(am5xy.XYChart.new(root, {
-panX: false,
-panY: false,
-wheelX: "panY",
-wheelY: "zoomY",
-layout: root.verticalLayout
-}));
-
-// Add scrollbar
-// https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
-chart.set("scrollbarY", am5.Scrollbar.new(root, {
-orientation: "vertical"
-}));
-var datos = [
-<?php
-                require 'conexionRh.php';
-
-                $sqlDM2 = $conexionGrafico->query("SELECT count(*) as totalDM2 from bajas where area2 = 'DIRECCION MEDICA'");
-                $rowDM2 = mysqli_fetch_assoc($sqlDM2);
-                $sqlDAF2 = $conexionGrafico->query("SELECT count(*) as totalDAF2 from bajas where area2 = 'DIRECCION DE ADMINISTRACION Y FINANZAS'");
-                $rowDAF2 = mysqli_fetch_assoc($sqlDAF2);
-                $sqlDG2 = $conexionGrafico->query("SELECT count(*) as totalDG2 from bajas where area2  = 'DIRECCION GENERAL'");
-                $rowDG2 = mysqli_fetch_assoc($sqlDG2);
-                $sqlDO2 = $conexionGrafico->query("SELECT count(*) as totalDO2 from bajas where area2  = 'DIRECCION DE OPERACIONES'");
-                $rowDO2 = mysqli_fetch_assoc($sqlDO2);
-                $sqlDEP2 = $conexionGrafico->query("SELECT count(*) as totalDEP2 from bajas where area2  = 'DIRECCION DE PLANEACION, ENSEÑANZA E INVESTIGACION'");
-                $rowDEP2 = mysqli_fetch_assoc($sqlDEP2);
-                ?>
-            ];
-var data = [ {
-"year": "Captura metas 2023",
-"D.G": <?php echo $rowDG2['totalDG2'] ?>,
-"D.M": <?php echo $rowDM2['totalDM2'] ?>,
-"D.A.F": <?php echo $rowDAF2['totalDAF2'] ?>,
-"D.O": <?php echo $rowDO2['totalDO2'] ?>,
-"D.P.E.E": <?php echo $rowDEP2['totalDEP2'] ?>
-}]
-
-
-// Create axes
-// https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
-var yRenderer = am5xy.AxisRendererY.new(root, {});
-var yAxis = chart.yAxes.push(am5xy.CategoryAxis.new(root, {
-categoryField: "year",
-renderer: yRenderer,
-tooltip: am5.Tooltip.new(root, {})
-}));
-
-yRenderer.grid.template.setAll({
-location: 1
-})
-
-yAxis.data.setAll(data);
-
-var xAxis = chart.xAxes.push(am5xy.ValueAxis.new(root, {
-min: 0,
-renderer: am5xy.AxisRendererX.new(root, {
-strokeOpacity: 0.1
-})
-}));
-
-// Add legend
-// https://www.amcharts.com/docs/v5/charts/xy-chart/legend-xy-series/
-var legend = chart.children.push(am5.Legend.new(root, {
-centerX: am5.p50,
-x: am5.p50
-}));
-
-
-// Add series
-// https://www.amcharts.com/docs/v5/charts/xy-chart/series/
-function makeSeries(name, fieldName) {
-var series = chart.series.push(am5xy.ColumnSeries.new(root, {
-name: name,
-stacked: true,
-xAxis: xAxis,
-yAxis: yAxis,
-baseAxis: yAxis,
-valueXField: fieldName,
-categoryYField: "year"
-}));
-
-series.columns.template.setAll({
-tooltipText: "{name}, {categoryY}: {valueX}",
-tooltipY: am5.percent(90)
-});
-series.data.setAll(data);
-
-// Make stuff animate on load
-// https://www.amcharts.com/docs/v5/concepts/animations/
-series.appear();
-
-series.bullets.push(function() {
-return am5.Bullet.new(root, {
-sprite: am5.Label.new(root, {
-text: "{valueX}",
-fill: root.interfaceColors.get("alternativeText"),
-centerY: am5.p50,
-centerX: am5.p50,
-populateText: true
-})
-});
-});
-
-legend.data.push(series);
-}
-
-makeSeries("D.G", "D.G");
-makeSeries("D.M", "D.M");
-makeSeries("D.A.F", "D.A.F");
-makeSeries("D.O", "D.O");
-makeSeries("D.P.E.E", "D.P.E.E");
-// Make stuff animate on load
-// https://www.amcharts.com/docs/v5/concepts/animations/
-chart.appear(1000, 100);
-
-}); // end am5.ready()
-</script>
-
-    <!-- HTML -->
-    <div id="chartdiv3"></div>
-
-</div>
-<div class="graff" id="grafico_barras">
-<style>
-#chartdiv4 {
+#chartdiv3 {
 width: 100%;
 height: 16rem;
 font-size: 12px;
@@ -475,7 +309,7 @@ am5.ready(function() {
 
 // Create root element
 // https://www.amcharts.com/docs/v5/getting-started/#Root_element
-var root = am5.Root.new("chartdiv4");
+var root = am5.Root.new("chartdiv3");
 
 
 var myTheme = am5.Theme.new(root);
@@ -614,6 +448,170 @@ chart.appear(1000, 100);
 }); // end am5.ready()
 </script>
 
+    <!-- HTML -->
+    <div id="chartdiv3"></div>
+
+</div>
+<div class="graff" id="grafico_barras">
+<style>
+#chartdiv4 {
+width: 100%;
+height: 16rem;
+}
+</style>
+<div class="titulo">
+        <h2 style="font-size: 13px;">Actualizacion datos medicos</h2>
+    </div>
+<!-- Resources -->
+<script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+
+<!-- Chart code -->
+<script>
+am5.ready(function() {
+
+// Create root element
+// https://www.amcharts.com/docs/v5/getting-started/#Root_element
+var root = am5.Root.new("chartdiv4");
+
+
+// Set themes
+// https://www.amcharts.com/docs/v5/concepts/themes/
+root.setThemes([
+am5themes_Animated.new(root)
+]);
+
+
+// Create chart
+// https://www.amcharts.com/docs/v5/charts/xy-chart/
+var chart = root.container.children.push(am5xy.XYChart.new(root, {
+panX: false,
+panY: false,
+wheelX: "panX",
+wheelY: "zoomX",
+layout: root.verticalLayout
+}));
+
+
+// Add legend
+// https://www.amcharts.com/docs/v5/charts/xy-chart/legend-xy-series/
+var legend = chart.children.push(
+am5.Legend.new(root, {
+centerX: am5.p50,
+x: am5.p50
+})
+);
+var datos = [
+
+<?php
+require 'conexionRh.php';
+
+
+$sqlplantilla = $conexionGrafico->query("SELECT count(*) as total from plantillahraei where plantillahraei.DescripcionPuesto like '%medico%' ");
+$rowplantilla = mysqli_fetch_assoc($sqlplantilla);
+
+
+$sqlSinactualizar = $conexionGrafico->query("SELECT count(*) as total2, actualizacion.actualizo from plantillahraei inner join actualizacion on actualizacion.id_empleado = plantillahraei.Empleado where plantillahraei.DescripcionPuesto like '%medico%' and actualizacion.actualizo = 0");
+$rowSinactualizar = mysqli_fetch_assoc($sqlSinactualizar);
+
+$sqlActualizo = $conexionGrafico->query("SELECT count(*) as total1, actualizacion.actualizo from plantillahraei inner join actualizacion on actualizacion.id_empleado = plantillahraei.Empleado where plantillahraei.DescripcionPuesto like '%medico%' and actualizacion.actualizo = 1");
+$rowActualizo = mysqli_fetch_assoc($sqlActualizo);
+
+
+
+?>
+];
+var data = [ {
+"year": "Total medicos",
+"Total medicos": <?php echo $rowplantilla['total'] ?>
+}, {
+"year": "Actualizo",
+"Actualizo": <?php echo $rowActualizo['total1'] ?>
+}, {
+"year": "Sin actualizar",
+"Sin actualizar": <?php echo $rowSinactualizar['total2'] ?>
+}]
+
+
+// Create axes
+// https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+var xRenderer = am5xy.AxisRendererX.new(root, {
+cellStartLocation: 0.1,
+cellEndLocation: 0.9
+})
+
+var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+categoryField: "year",
+renderer: xRenderer,
+tooltip: am5.Tooltip.new(root, {})
+}));
+
+xRenderer.grid.template.setAll({
+location: 1
+})
+
+xAxis.data.setAll(data);
+
+var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+renderer: am5xy.AxisRendererY.new(root, {
+strokeOpacity: 0.1
+})
+}));
+
+
+// Add series
+// https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+function makeSeries(name, fieldName) {
+var series = chart.series.push(am5xy.ColumnSeries.new(root, {
+name: name,
+xAxis: xAxis,
+yAxis: yAxis,
+valueYField: fieldName,
+categoryXField: "year"
+}));
+
+series.columns.template.setAll({
+tooltipText: "{name}, {categoryX}:{valueY}",
+width: am5.percent(90),
+tooltipY: 0,
+strokeOpacity: 0
+});
+
+series.data.setAll(data);
+
+// Make stuff animate on load
+// https://www.amcharts.com/docs/v5/concepts/animations/
+series.appear();
+
+series.bullets.push(function() {
+return am5.Bullet.new(root, {
+locationY: 0,
+sprite: am5.Label.new(root, {
+text: "{valueY}",
+fill: root.interfaceColors.get("alternativeText"),
+centerY: 0,
+centerX: am5.p50,
+populateText: true
+})
+});
+});
+
+legend.data.push(series);
+}
+
+makeSeries("Total medicos", "Total medicos");
+makeSeries("Actualizo", "Actualizo");
+makeSeries("Sin actualizar", "Sin actualizar");
+
+
+// Make stuff animate on load
+// https://www.amcharts.com/docs/v5/concepts/animations/
+chart.appear(1000, 100);
+
+}); // end am5.ready()
+</script>
+
 <!-- HTML -->
 <div id="chartdiv4"></div>
 </div>
@@ -626,7 +624,7 @@ height: 16rem;
 }
 </style>
 <div class="titulo">
-        <h2 style="font-size: 13px;">Estatus actualizacion de datos</h2>
+        <h2 style="font-size: 13px;">Estatus actualizacion de datos general</h2>
     </div>
 <!-- Resources -->
 <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
@@ -790,7 +788,7 @@ height: 16rem;
 }
 </style>
 <div class="titulo">
-        <h2 style="font-size: 13px;">Estatus actualizacion de datos</h2>
+        <h2 style="font-size: 13px;">Actualizacion datos enfermeria</h2>
     </div>
 <!-- Resources -->
 <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
