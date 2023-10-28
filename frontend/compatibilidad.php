1585461ab7f697d720f5603d97201b59c5699faa
Talento RRHH <talentorh@hraei.gob.mx>
2023-10-27 18:44:04 -0600

    carga de informacion actualizada

--- frontend/compatibilidad.php
+++ frontend/compatibilidad.php
@@ -42,7 +42,32 @@
 
 
     </nav>
+    <?php
+            error_reporting(0);
+    switch(true) {
 
+        case isset($_SESSION['usuarioAdminRh']):
+            $nombreempleado = $row['Nombre'];
+            $identificador = $row['Empleado'];
+        break;
+        
+        case isset($_SESSION['usuarioJefe']):
+            $nombreempleado = $row['Nombre'];
+            $identificador = $row['Empleado'];
+        break;
+
+        case isset($_SESSION['usuarioDatos']):
+            $nombreempleado = $row['Nombre'];
+            $identificador = $row['Empleado'];
+        break;
+
+        default:
+        
+        require 'close_sesion.php';
+        
+        }
+        
+?>
     <div class="container">
         <div id="mensaje"></div>
         <h1 style="text-align: center; font-size: 25px;">Carga de información de compatibilidad laboral</h1>
@@ -73,6 +98,13 @@
             <script>
 $(function () {
     $('#contenido').prop("hidden", true);
+    let val = $('#validaotroempleo').val();
+
+    if(val == 'Si'){
+        $('#contenido').prop("hidden", false);
+    }else{
+        $('#contenido').prop("hidden", true);
+    }
 
 })
 $(document).ready(function () {
@@ -87,45 +119,36 @@ $('#otroempleo').change(function (e) {
     }
 })
 });
+
 </script>
+<?php 
+require 'clases/conexion.php';
+$conexionX = new ConexionRh();
+        $sqlC = $conexionX->prepare("SELECT * from compatibilidadotroempleo where id_empleado = :id_empleado");
+            $sqlC->execute(array(
+                ':id_empleado'=>$identificador
+            ));
+        $rowC = $sqlC->fetch();
+        $validacion = $rowC['otroempleo'];
+
+        $sql = $conexionX->prepare("SELECT * from compatibilidad where id_empleado = :id_empleado");
+        $sql->execute(array(
+            ':id_empleado'=>$identificador
+        ));
+    $row = $sql->fetch();
+
+?>
+<input type="hidden" value="<?php echo $validacion ?>" id="validaotroempleo">
 <div class="col-md-6">
                     <strong>Cuento con otro empleo:</strong>
                     <select name="otroempleo" id="otroempleo" class="form-control" required>
-                        <option value="">Seleccione</option>
+                        <option value="<?php echo $rowC['otroempleo'] ?>"><?php echo $rowC['otroempleo'] ?></option>
                         <option value="Si">Si</option>
                         <option value="No">No</option>
                 
             </select>
                 </div>
             
-            <?php
-            error_reporting(0);
-    switch(true) {
-
-        case isset($_SESSION['usuarioAdminRh']):
-            $nombreempleado = $row['Nombre'];
-            $identificador = $row['Empleado'];
-        break;
-        
-        case isset($_SESSION['usuarioJefe']):
-            $nombreempleado = $row['Nombre'];
-            $identificador = $row['Empleado'];
-        break;
-
-        case isset($_SESSION['usuarioDatos']):
-            $nombreempleado = $row['Nombre'];
-            $identificador = $row['Empleado'];
-        break;
-
-        default:
-        
-        require 'close_sesion.php';
-        
-        }
-        
-?>
-
-
 <div id="contenido">
 <div class="form-row">
 <div class="col-md-6">
@@ -142,19 +165,20 @@ $('#otroempleo').change(function (e) {
 
                 <div class="col-md-6">
                     <label for="mensaje">Nombre de la institución donde labora:</label>
-                    <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre de la institución" >
+                    <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre de la institución" value="<?php echo $row['LUGARDETRABAJO'] ?>" >
                 </div>
                 <div class="col-md-4">
                     <label for="mensaje">Horario:</label>
-                    <input type="text" class="form-control" name="horario" id="horario" placeholder="Horario en el que labora">
+                    <input type="text" class="form-control" name="horario" id="horario" placeholder="Horario en el que labora" value="<?php echo $row['HORARIO'] ?>">
                 </div>
                 <div class="col-md-4">
                     <label for="mensaje">Dias:</label>
-                    <input type="text" class="form-control" name="dias" id="dias" placeholder="Dias en los que labora">
+                    <input type="text" class="form-control" name="dias" id="dias" placeholder="Dias en los que labora" value="<?php echo $row['DIASLABORALES'] ?>">
                 </div>
                 <div class="col-md-4">
                 <label for="mensaje">Tipo de puesto que ocupa:</label>
                 <select name="tipopuesto" id="tipopuesto" class="form-control">
+                    <option value="<?php echo $row['tipopuesto'] ?>"><?php echo $row['tipopuesto'] ?></option>
                 <option value="Ninguno">Seleccione</option>
                 <option value="Confianza">Confianza</option>
                 <option value="Base">Base</option>
@@ -162,6 +186,7 @@ $('#otroempleo').change(function (e) {
                 <option value="Provisional">Provisional</option>
                 <option value="Provisional Reservada">Provisional Reservada</option>
                 <option value="Interinato">Interinato</option>
+                <option value="Honorarios">Honorarios</option>
             </select>
                 </div>
 
@@ -172,19 +197,20 @@ $('#otroempleo').change(function (e) {
 
                 <div class="col-md-6">
                     <label for="mensaje">Nombre de la institución donde labora Segundo:</label>
-                    <input type="text" class="form-control" name="nombreinstitucionsegundo" id="nombreinstitucionsegundo" placeholder="Nombre de la institución">
+                    <input type="text" class="form-control" name="nombreinstitucionsegundo" id="nombreinstitucionsegundo" placeholder="Nombre de la institución" value="<?php echo $row['LUGARDETRABAJO2'] ?>">
                 </div>
                 <div class="col-md-6">
                     <label for="mensaje">Horario Segundo:</label>
-                    <input type="text" class="form-control" name="horariosegundo" id="horariosegundo" placeholder="Horario en el que labora">
+                    <input type="text" class="form-control" name="horariosegundo" id="horariosegundo" placeholder="Horario en el que labora" value="<?php echo $row['HORARIO2'] ?>">
                 </div>
                 <div class="col-md-6">
                     <label for="mensaje">Dias Segundo:</label>
-                    <input type="text" class="form-control" name="diassegundo" id="diassegundo" placeholder="Dias en los que labora">
+                    <input type="text" class="form-control" name="diassegundo" id="diassegundo" placeholder="Dias en los que labora" value="<?php echo $row['DIASLABORALES2'] ?>"> 
                 </div>
                 <div class="col-md-6">
                 <label for="mensaje">Tipo de puesto que ocupa Segundo:</label>
                 <select name="tipopuestosegundo" id="tipopuestosegundo" class="form-control">
+                <option value="<?php echo $row['tipopuesto2'] ?>"><?php echo $row['tipopuesto2'] ?></option>
                 <option value="Ninguno">Seleccione</option>
                 <option value="Confianza">Confianza</option>
                 <option value="Base">Base</option>
@@ -192,6 +218,7 @@ $('#otroempleo').change(function (e) {
                 <option value="Provisional">Provisional</option>
                 <option value="Provisional Reservada">Provisional Reservada</option>
                 <option value="Interinato">Interinato</option>
+                <option value="Honorarios">Honorarios</option>
             </select>
                 </div>
 </div>
