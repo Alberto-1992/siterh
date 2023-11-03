98321f6e4fb25083e17854d0c56028c774bff9e8
Talento RRHH <talentorh@hraei.gob.mx>
2023-11-03 13:10:31 -0600

    carga de datos a git

--- aplicacion/eliminarRegistroCandidato.php
+++ aplicacion/eliminarRegistroCandidato.php
@@ -5,7 +5,6 @@ date_default_timezone_set("America/Monterrey");
 $id = $_POST['id'];
 $identificador = $_POST['curp'];
 $hora = date("Y-m-d h:i:sa");
-
 try {
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conexion->setAttribute(PDO::ATTR_AUTOCOMMIT, 0);
