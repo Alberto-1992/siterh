9aaff812d9cfcdede410996850427019513caf0b
Alberto Vazquez Arias <talentorh@hraei.gob.mx>
2023-12-22 17:37:06 -0600

    reporte se agrega fecha de validacion

--- reportes/reporteCapacitacion.php
+++ reportes/reporteCapacitacion.php
@@ -19,6 +19,7 @@
     //columnas del archivo , llamar a la funcion fputcsv
     fputcsv($salida, array(
         mb_convert_encoding('N° empleado', 'ISO-8859-1', 'UTF-8'),
+        mb_convert_encoding('Fecha de validacion', 'ISO-8859-1', 'UTF-8'),
         mb_convert_encoding('Año', 'ISO-8859-1', 'UTF-8'),
         mb_convert_encoding('Tipo de capacitación', 'ISO-8859-1', 'UTF-8'),
             'Nombre del curso',
@@ -42,20 +43,24 @@
 
     $conexionX = new ConexionRh();
 
- $sql = $conexionX->prepare("SELECT id,nombrecurso,catalogoprograma,lineaestrategica,competenciaalieandaeje,id_empleado,fechacriteriotermino,fechacriterioinicio,criteriocurso,fechainicio,fechatermino,modalidad,horas,asistecomo,nombreinstitucion,otroexpidedocumento,tipocapacitacion,documentorecibe, EXTRACT(YEAR 
+ $sql = $conexionX->prepare("SELECT id,nombrecurso,catalogoprograma,lineaestrategica,competenciaalieandaeje,id_empleado,fechacriteriotermino,fechacriterioinicio,criteriocurso,fechainicio,fechatermino,modalidad,horas,asistecomo,nombreinstitucion,otroexpidedocumento,tipocapacitacion,documentorecibe,fechavalidacion, EXTRACT(YEAR 
  FROM fechatermino) as anio from datos order by fechainicio desc");
  $sql->execute();
     
     while($filaR = $sql->fetch()){
-    fputcsv($salida, array(
+        $fechavalidacion = date("d-m-Y", strtotime($filaR['fechavalidacion']));
+                    $fechainicionew = date("d-m-Y", strtotime($filaR['fechainicio']));
+                    $fechaterminonew = date("d-m-Y", strtotime($filaR['fechatermino']));
+                        fputcsv($salida, array(
                         $filaR['id_empleado'],
+                        $fechavalidacion,
                         $filaR['anio'],
                         mb_convert_encoding($filaR['tipocapacitacion'], 'ISO-8859-1', 'UTF-8'),
                         mb_convert_encoding($filaR['nombrecurso'], 'ISO-8859-1', 'UTF-8'),
                         $filaR['horas'],
                         mb_convert_encoding($filaR['modalidad'], 'ISO-8859-1', 'UTF-8'),
-                        mb_convert_encoding($filaR['fechainicio'], 'ISO-8859-1', 'UTF-8'),
-                        mb_convert_encoding($filaR['fechatermino'], 'ISO-8859-1', 'UTF-8'),
+                        mb_convert_encoding($fechainicionew, 'ISO-8859-1', 'UTF-8'),
+                        mb_convert_encoding($fechaterminonew, 'ISO-8859-1', 'UTF-8'),
                         mb_convert_encoding($filaR['asistecomo'], 'ISO-8859-1', 'UTF-8'),
                         mb_convert_encoding($filaR['documentorecibe'], 'ISO-8859-1', 'UTF-8'),
                         mb_convert_encoding($filaR['catalogoprograma'], 'ISO-8859-1', 'UTF-8'),
