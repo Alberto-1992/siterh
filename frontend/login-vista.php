60424a841ac7b55d740ba4393ef3f3aecb266d2a
Alberto Vazquez Arias <talentorh@hraei.gob.mx>
2023-12-26 01:17:54 -0600

    carga de usuario logueado

--- frontend/login-vista.php
+++ frontend/login-vista.php
@@ -49,7 +49,7 @@
 </html>
 <?php session_start();
 error_reporting(0);
-$ip = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     
     $correo = $_POST['usuario'];
