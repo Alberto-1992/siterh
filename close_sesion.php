a081a06e0f977559a574b90f67cac6125b1e7349
EquipoDELLingeBeto <talentorh@hraei.gob.mx>
2023-12-28 23:29:39 -0600

    carga de archivos ignorados

--- close_sesion.php
+++ close_sesion.php
@@ -1,8 +1,14 @@
 <?php session_start();
+require 'clases/conexion.php';
+$conexion = new ConexionRh();
+$usernameSesion = $_SESSION['usuarioAdminRh']; 
+    $sql = $conexion->prepare("UPDATE usuariosrh set logueado = :logueado where correoelectronico = :correoelectronico");
+        $sql->execute(array(
+            ':logueado'=>0,
+            ':correoelectronico'=>$usernameSesion
+        ));
 $_SESSION = array();
 session_destroy();
-
     header('location: index');
-    exit;
-
+exit;
 ?>
