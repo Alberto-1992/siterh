b9adbb615b2f29d87301ca96a3b9b6660182dce3
EquipoDELLingeBeto <talentorh@hraei.gob.mx>
2023-12-30 14:23:31 -0600

    carga de horarios

--- consultaBusquedaPlantillaHraei.php
+++ consultaBusquedaPlantillaHraei.php
@@ -8,8 +8,8 @@ $id = $_POST['id'];
     $conexionX->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conexionX->setAttribute(PDO::ATTR_AUTOCOMMIT, 0);
     $conexionX->beginTransaction();
-$query= $conexionX->prepare("SELECT plantillahraei.*, horariosplantilla.*, compatibilidad.* from plantillahraei inner join horariosplantilla on horariosplantilla.Empleado = plantillahraei.Empleado
-inner join compatibilidad on compatibilidad.id_empleado = plantillahraei.Empleado
+$query= $conexionX->prepare("SELECT plantillahraei.*, horariosplantilla.*, compatibilidad.* from plantillahraei left outer join horariosplantilla on horariosplantilla.Empleado = plantillahraei.Empleado
+left outer join compatibilidad on compatibilidad.id_empleado = plantillahraei.Empleado
 where plantillahraei.Empleado = $id");
 $query->execute();
 $dataRegistro= $query->fetch();
