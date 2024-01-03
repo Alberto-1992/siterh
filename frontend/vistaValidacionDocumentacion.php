dc68e72fc62fafe45a7024c714df3eaef1d6ebba
EquipoDELLingeBeto <talentorh@hraei.gob.mx>
2024-01-03 11:59:23 -0600

    carga de arbol de archivos

--- frontend/vistaValidacionDocumentacion.php
+++ frontend/vistaValidacionDocumentacion.php
@@ -728,7 +728,7 @@ $nombrecurso = $dataRegistro['nombrecurso'];
             <div class="accordion-item">
                 <h2 class="accordion-header" id="headingOne">
                     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTres" aria-expanded="true" aria-controls="collapseTres">
-                        Archivos 2022
+                        Archivos 2021
                     </button>
                 </h2>
                 <div id="collapseTres" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
